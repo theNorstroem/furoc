ba0c7514661014134d8777f2d1c3e70b17eb112a
veith <veithz@gmail.com>
2020-09-25 12:18:34 +0200

    doc: sample explained

--- sample/furoc-gen-sample/main.go
+++ sample/furoc-gen-sample/main.go
@@ -7,29 +7,38 @@ import (
 )
 
 func main() {
+
+	// receive the request, all the stdin and mapping stuff is done here
+	// if you need to debug your plugin, use following arguments:
+	// debug debugfile=./sample/fullyaml.yaml
 	req := furoc.NewRequester()
+
+	// create a responser, which can be used to add files and send the response back to furoc
 	res := furoc.NewResponser()
 
-	//req.Fprintln("Start")
+	// use req.Fprintln(interface{}) if you want to print something to the console (or write to stderr)
+	// you can not write to the console with fmt or log (because this goes to stdout)
+	req.Fprintln("Sample plugin started")
 
 	// for convinience
 	ast := req.AST
 
 	for name, s := range ast.Services {
-		//req.Fprintln(name)
+
 		c, err := yaml.Marshal(s.ServiceSpec)
 		if err != nil {
 			log.Fatal(err)
 		}
-		// sample file
+		// create sample file
 		readme := furoc.TargetFile{
 			Filename: "/" + name + "/" + s.ServiceSpec.Name + ".md",
 			Content:  c,
 		}
 
-		// build a sample reqres
+		// Add file to the responder
 		res.AddFile(&readme)
 	}
 
+	// send the response back to furoc
 	res.SendResponse()
 }
